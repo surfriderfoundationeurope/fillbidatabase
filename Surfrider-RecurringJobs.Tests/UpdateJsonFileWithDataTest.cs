aa96799ad275f04c70a901afbc2b06c569dcb3a6
Christophe Havard <havard.christophe@gmail.com>
2020-10-21 13:59:53 +0200

    added test project moved src code to dedicated folder

---
+++ Surfrider-RecurringJobs.Tests/UpdateJsonFileWithDataTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Surfrider_RecurringJobs.Tests
+{
+    [TestClass]
+    public class UpdateJsonFileWithDataTest
+    {
+        [TestMethod]
+        public void UpdateJsonFileWithData_ServerNotRespond()
+        {
+            Console.WriteLine("Test 1");
+        }
+    }
+}
