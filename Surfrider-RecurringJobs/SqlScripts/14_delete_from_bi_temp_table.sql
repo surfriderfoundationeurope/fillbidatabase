d3d4fd022b74edd25fba9bb3bf271076651bf8e0
baccar clement <clementb@namr.com>
2020-12-23 11:21:52 +0100

    remove select query

--- Surfrider-RecurringJobs/SqlScripts/14_delete_from_bi_temp_table.sql
+++ Surfrider-RecurringJobs/SqlScripts/14_delete_from_bi_temp_table.sql
@@ -2,11 +2,6 @@
 This script delete data in bi_temp schema
 */
 
-SELECT campaign_id
-FROM bi_temp.pipelines
-WHERE campaign_has_been_computed = True AND river_has_been_computed = True
-;
-
 DELETE FROM bi_temp.trash WHERE pipeline_id in (@pipelineID);
 DELETE FROM bi_temp.campaign WHERE pipeline_id in (@pipelineID);
 DELETE FROM bi_temp.trajectory_point WHERE pipeline_id in (@pipelineID);
