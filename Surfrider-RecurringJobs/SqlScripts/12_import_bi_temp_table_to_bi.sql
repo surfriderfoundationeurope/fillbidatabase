159bc532d3efefcad1e01a814fe7104101941ce7
clementb <clementb@namr.com>
2021-04-26 23:06:17 +0200

    fix query 7

--- Surfrider-RecurringJobs/SqlScripts/12_import_bi_temp_table_to_bi.sql
+++ Surfrider-RecurringJobs/SqlScripts/12_import_bi_temp_table_to_bi.sql
@@ -7,7 +7,7 @@ insert into bi.campaign (id,locomotion,isaidriven,remark,id_ref_user_fk,riversid
 select
 id, locomotion, isaidriven, remark, id_ref_user_fk, riverside, start_date, end_date, start_point, end_point, total_distance, avg_speed, duration, start_point_distance_sea, end_point_distance_sea, trash_count, distance_start_end, id_ref_model_fk, createdon
 from bi_temp.campaign
-where pipeline_id in (@pipelineID) AND id in (@campaign_ids)
+where pipeline_id in (@pipelineID) AND id in (@campaignID)
 ;
 
 -- QUERY 2: migration for table capaign_river
@@ -15,7 +15,7 @@ insert into bi_temp.campaign_river (id, id_ref_campaign_fk, river_name, id_ref_r
 select
 id, id_ref_campaign_fk, river_name, id_ref_river_fk, distance, the_geom, createdon
 from bi_temp.campaign_river
-where pipeline_id  in (@pipelineID) AND id_ref_campaign_fk in (@campaign_ids)
+where pipeline_id  in (@pipelineID) AND id_ref_campaign_fk in (@campaignID)
 ;
 
 -- QUERY 3: migration for table trajectory_point
@@ -23,7 +23,7 @@ insert into bi.trajectory_point (id, the_geom, id_ref_campaign_fk, elevation, di
 select
 id, the_geom, id_ref_campaign_fk, elevation, distance, time_diff, time, speed, lat, lon, createdon
 from bi_temp.trajectory_point tp
-where pipeline_id in (@pipelineID) AND id_ref_campaign_fk in (@campaign_ids)
+where pipeline_id in (@pipelineID) AND id_ref_campaign_fk in (@campaignID)
 ;
 
 -- QUERY 4: migration for table trajectory_point_river
@@ -31,7 +31,7 @@ insert into bi.trajectory_point_river (id, id_ref_trajectory_point_fk, id_ref_ca
 select
 id, id_ref_trajectory_point_fk, id_ref_campaign_fk, id_ref_river_fk, trajectory_point_the_geom, river_the_geom, closest_point_the_geom, distance_river_trajectory_point, projection_trajectory_point_river_the_geom, importance, river_name, createdon
 from bi_temp.trajectory_point_river
-where pipeline_id in (@pipelineID) AND id_ref_campaign_fk in (@campaign_ids)
+where pipeline_id in (@pipelineID) AND id_ref_campaign_fk in (@campaignID)
 ;
 
 -- QUERY 5: migration for table trash
@@ -57,5 +57,10 @@ set count_trash = bi_temp_r.count_trash ,
     the_geom_monitored = bi_temp_r.the_geom_monitored ,
     trash_per_km = bi_temp_r.trash_per_km
 from bi_temp.river bi_temp_r
-where bi_temp_r.name = bi_temp_r.name AND id_ref_campaign_fk in (@campaignID) AND pipeline_id in (@pipelineID)
+where bi_temp_r.name = r.name AND bi_temp_r.name in (
+                                                     SELECT river_name
+						                             FROM bi_temp.campaign_river
+					                                 WHERE id_ref_campaign_fk IN (@campaignID)
+							                                  AND pipeline_id IN (@pipelineID)
+						                             )
 ;
